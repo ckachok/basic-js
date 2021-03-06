cc05e197cc0050442a710656b3e3618e516779ba
ckachok <mefelion89@gmail.com>
2021-03-07 00:20:41 +0300

    feat: add a solution to the task simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chainLinks: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chainLinks.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    arguments.length === 0 ? 
+    this.chainLinks.push(`( )`) : 
+    this.chainLinks.push(`( ${value} )`);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position !== 'number' || 
+        !Number.isInteger(position) ||
+        position < 1 ||
+        position > this.getLength()) {
+          this.chainLinks.length = 0;
+          throw new Error('Position is not integer number or out of range');
+    }
+    this.chainLinks.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainLinks.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const chain = this.chainLinks.join('~~');
+    this.chainLinks.length = 0;
+    return chain;
   }
 };
 
 module.exports = chainMaker;
+
